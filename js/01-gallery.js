0d7dfec1ba03c5e03aad41d929fafcef4f395f4a
Kinderpro <131799650+Kinderpro@users.noreply.github.com>
2023-09-04 22:14:20 +0300

    task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery')
+
+const markup = creatMarkup(galleryItems)
+
+container.insertAdjacentHTML('beforeend', markup);
+container.addEventListener('click', onClick);
+
+function creatMarkup (arr) {
+    return arr.map(({ original, description }) => {
+        return `<li class="gallery__item">
+        <a href="${original}" class="gallery__link">
+          <img class="gallery__image" src="${original}" alt="${description}" data-source="${original}">
+        </a>
+      </li>`
+    }).join('');
+};
+
+function onClick (evt) {
+   if (evt.target === evt.currentTarget) {
+    return;
+   }
+evt.preventDefault();
+
+const targetEl = evt.target.closest(".gallery__image");
+
+const instance = basicLightbox.create(
+    `<div class="modal">
+    <img src="${targetEl.dataset.source}" alt="${targetEl.alt}"/>
+    </div>` 
+);
+
+instance.show();
+};
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const container = document.querySelector('.gallery')
+
+const markup = creatMarkup(galleryItems)
+
+container.insertAdjacentHTML('beforeend', markup);
+container.addEventListener('click', onClick);
+
+function creatMarkup (arr) {
+    return arr.map(({ original, description }) => {
+        return `<li class="gallery__item">
+        <a href="${original}" class="gallery__link">
+          <img class="gallery__image" src="${original}" alt="${description}" title="${description}">
+        </a>
+      </li>`
+    }).join('');
+};
+
+function onClick (evt) {
+   if (evt.target === evt.currentTarget) {
+    return;
+   }
+evt.preventDefault();
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250
+});
+ 
+};
 console.log(galleryItems);
